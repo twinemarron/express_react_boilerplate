29488af2047a14ba857dee5cfbc1cb7c5a382516
twinemarron <twinemarron@gmail.com>
2018-10-14 12:42:25 +0900

    update

--- src/routes/serverRoutes.tsx
+++ src/routes/serverRoutes.tsx
@@ -8,13 +8,21 @@ import { renderRoutes } from 'react-router-config'
 
 import routes from './routes'
 
+// console.log('routes == : ', routes)
+// console.log('renderRoutes(routes) : ', renderRoutes(routes))
+
 const router = express.Router()
+
+const Routes = () => renderRoutes(routes)
 
 router.get('*', (req, res) => {
   let context = {}
   ReactDOMServer.renderToNodeStream(
     <StaticRouter location={req.url} context={context}>
-      {renderRoutes(routes)}
+      {
+        // renderRoutes(routes)
+      }
+      <Routes />
     </StaticRouter>
   ).pipe(res)
 })
